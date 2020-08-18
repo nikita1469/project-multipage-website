0e72b27a5f06a55fed3cbf6f8ee1b3609254cb40
nikita1469 <pogrebnoi.nikita2505@gmail.com>
2020-08-18 19:27:03 +0300

    video player with youtube api

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from './modules/slider'; 
+import VideoPlayer from './modules/videoPlayer';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
 })
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,6 +15,19 @@ export default class Slider {
             this.slideIndex = this.slide.length;
         }
 
+        try {
+            this.hanson.style.opacity = '0';
+            if(n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch(e) {};
+
         this.slides.forEach(slide => { // убрали display: block у всех элементов слайдера
             slide.style.display = 'none';
         });
@@ -27,6 +40,10 @@ export default class Slider {
     }
 
     render() {
+        try {          
+            this.hanson = document.querySelector('.hanson');
+        } catch(e) {};
+        
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
